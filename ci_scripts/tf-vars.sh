9e2488204cb0796e00b0438bfa9fb1e969671dc2
hmen97 <hmenezes36@gmail.com>
2021-06-11 16:57:50 +0530

    changed BASEL_VC variable path for windows tf build

--- ci_scripts/tf-vars.sh
+++ ci_scripts/tf-vars.sh
@@ -37,7 +37,7 @@ elif [ "${OS}" = "${CI_MSYS_VERSION}" ]; then
     export CI_ARTIFACTS_DIR="$(cygpath ${CI_ARTIFACTS_DIR})"
 
     export DS_ROOT_TASK=${CI_TASK_DIR}
-    export BAZEL_VC="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC"
+    export BAZEL_VC="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC"
     export BAZEL_VC_FULL_VERSION="14.28.29910"
     export MSYS2_ARG_CONV_EXCL='//'
 
